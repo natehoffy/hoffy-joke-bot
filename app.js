7e657440f34a53374bda73e18e18f0062615dc7d
Nate Hoffelmeyer <nhoffelmeyer@hey.com>
2021-12-29 11:57:01 -0700

    deploy 4

--- app.js
+++ app.js
@@ -7,5 +7,10 @@ const PORT = config.PORT
 
 app.use(express.static("public"))
 app.use(bodyParser.json())
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
+});
+app.post("/", webhookRouter.router)
 app.listen(PORT || 4040, () => console.log(`Testing app listening on port ${PORT}`))
-app.use("/", webhookRouter.router)
