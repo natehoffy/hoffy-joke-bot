b774a26a8a74d63e67902a8d621c4c4a55d787e8
Nate Hoffelmeyer <nhoffelmeyer@hey.com>
2021-12-29 11:59:40 -0700

    deploy 5

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const webhookRouter = require("./routes/webhookRouter")
+//const webhookRouter = require("./routes/webhookRouter")
+const {processWebhook} = require("../controllers/webhookController")
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
@@ -12,5 +13,6 @@ app.get('/', function(req, res) {
     // ejs render automatically looks in the views folder
     res.render('index');
 });
-app.post("/", webhookRouter.router)
+//app.post("/", webhookRouter.router)
+app.post("/", processWebhook)
 app.listen(PORT || 4040, () => console.log(`Testing app listening on port ${PORT}`))
